b71fd12bb3f075beee66facb6e61d9c838288229
DanielKoh2004 <105713982+DanielKoh2004@users.noreply.github.com>
2022-07-04 07:27:45 +0800

    Add files via upload

--- work/Program3.py
+++ work/Program3.py
@@ -1,7 +1,7 @@
-num1 = float(input("Enter amount invested in SPY:"))
-num2 = float(input("Enter amount invested in QQQ:"))
-num3 = float(input("Enter amount invested in EEM:"))
-num4 = float(input("Enter amount invested in VXX:"))
+num1 = float(input("Enter amount invested in SPY: "))
+num2 = float(input("Enter amount invested in QQQ: "))
+num3 = float(input("Enter amount invested in EEM: "))
+num4 = float(input("Enter amount invested in VXX: "))
 
 total = num1 + num2 + num3 + num4
 SPYperc = num1/total
@@ -11,5 +11,8 @@ VXXperc = num4/total
 
 print("ETF      PERCENTAGE")
 print("-------------------")
-print("SPY      ",num1%)
-print("QQQ      "
+print("SPY     {0:10,.2%}".format(SPYperc))
+print("QQQ     {0:10,.2%}".format(QQQperc))
+print("EEM     {0:10,.2%}".format(EEMperc))
+print("VXX     {0:10,.2%}".format(VXXperc))
+print("TOTAL AMOUNT INVESTED : $",total)
